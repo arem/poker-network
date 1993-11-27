dcc43e641208fee16346d51663f4bb3cb4c0af2f
ctm <ctm@c4f34931-41c7-4605-b615-9bb9f479c57e>
1993-11-27 08:05:18 +0000

    Initial revision
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@13 c4f34931-41c7-4605-b615-9bb9f479c57e

---
+++ ctm-eval/lib/helper.c
@@ -0,0 +1,69 @@
+#include "poker.h"
+
+static void dump_rank( int ranks, char suitchar )
+{
+    int bit;
+    const char *namep;
+    static char names[] = "AKQJT98765432";
+
+    for (bit = 1 << (N_RANK - 1), namep = names; bit; bit >>= 1, ++namep) {
+	if (ranks & bit) {
+	    putchar(*namep);
+	    putchar(suitchar);
+	    putchar(' ');
+	}
+    }
+}
+
+static void dump_cards( cards_u cards )
+{
+    dump_rank(cards.cards_t.hearts,   'H');
+    dump_rank(cards.cards_t.diamonds, 'D');
+    dump_rank(cards.cards_t.spades,   'S');
+    dump_rank(cards.cards_t.clubs,    'C');
+    putchar('\n');
+}
+
+static const char *hand_names[] = {
+    "internal error",
+    "high hand",
+    "pair",
+    "two pair",
+    "three of a kind",
+    "straight",
+    "flush",
+    "full house",
+    "four of a kind",
+    "straight flush",
+};
+
+static void dump_eval( eval_u eval )
+{
+    static const char *rank_names[] = {
+	"deuce",
+	"trey",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+	"ten",
+	"jack",
+	"queen",
+	"king",
+	"ace",
+    };
+    printf("%s: %s", hand_names[eval.eval_t.hand],
+					     rank_names[eval.eval_t.top_card]);
+    if (eval.eval_t.second_card)
+	printf(", %s", rank_names[eval.eval_t.second_card]);
+    if (eval.eval_t.third_card)
+	printf(", %s", rank_names[eval.eval_t.third_card]);
+    if (eval.eval_t.fourth_card)
+	printf(", %s", rank_names[eval.eval_t.fourth_card]);
+    if (eval.eval_t.fifth_card)
+	printf(", %s", rank_names[eval.eval_t.fifth_card]);
+
+    printf("\n");
+}
