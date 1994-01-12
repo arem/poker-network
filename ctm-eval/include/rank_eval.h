200470196386c939120212d8755394e9cd412cfe
ctm <ctm@c4f34931-41c7-4605-b615-9bb9f479c57e>
1994-01-12 21:11:42 +0000

    Keith speedups.
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@49 c4f34931-41c7-4605-b615-9bb9f479c57e

--- ctm-eval/include/rank_eval.h
+++ ctm-eval/include/rank_eval.h
@@ -1,14 +1,17 @@
 /*
  *  rank_eval.h: a fast poker hand evaluator that doesn't care about
- *		 signficant cards (i.e. it only figures out the rank
- *		 of a hand).
+ *                 signficant cards (i.e. it only figures out the rank
+ *                 of a hand).
  *
- *  Copyright (C) 1993  Clifford T. Matthews
+ *  Copyright (C) 1994  Clifford T. Matthews, Keith Miyake
  *
  *  Roy T. Hashimoto (hashimoto@sgi.com) is the inspiration for this
- *	evaluator.  Roy wrote the original fish.c, which had a Finite
- *	State Machine (FSM) based evaluator in it.  The code below doesn't
- *	look like Roy's, but the idea is the same.
+ *        evaluator.  Roy wrote the original fish.c, which had a Finite
+ *        State Machine (FSM) based evaluator in it.  The code below doesn't
+ *        look like Roy's, but the idea is the same.
+ *
+ *  Keith Miyake (kmiyake@cs.rutgers.edu) rearranged things to squeak
+ *	some more speed out of this code.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -25,8 +28,8 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-#if	!defined(__FISH_EVAL__)
-#define	__FISH_EVAL__
+#if        !defined(__FISH_EVAL__)
+#define        __FISH_EVAL__
 
 #include "poker.h"
 
@@ -35,7 +38,7 @@ static inline hand_t rank_eval(uint64 hand)
     hand_t retval, fsm_hand;
     unsigned long long n;
     uint32 hearts, diamonds, clubs, spades;
-	
+        
 
     hearts   = hand & SUIT_MASK;
     hand   >>= SUIT_SHIFT;
@@ -48,41 +51,36 @@ static inline hand_t rank_eval(uint64 hand)
 
     spades   = hand;
 
-    retval = str_and_flu_table[high_hand][spades  ];
-    retval = str_and_flu_table[retval   ][clubs   ];
-    retval = str_and_flu_table[retval   ][diamonds];
-    retval = str_and_flu_table[retval   ][hearts  ];
+    n = cards_to_counts_table[spades  ] +
+        cards_to_counts_table[clubs   ] +
+        cards_to_counts_table[diamonds] +
+        cards_to_counts_table[hearts  ];
 
-    if (retval < straight_flush) {
-	n = cards_to_counts_table[spades  ] +
-	    cards_to_counts_table[clubs   ] +
-	    cards_to_counts_table[diamonds] +
-	    cards_to_counts_table[hearts  ];
+    retval = straight_table[spades   |
+                            clubs    |
+                            diamonds |
+                            hearts     ];
 
-        fsm_hand = straight_table[spades   |
-				  clubs    |
-				  diamonds |
-				  hearts     ];
+    retval = str_and_flu_table[retval][hearts  ];
+    retval = str_and_flu_table[retval][clubs   ];
+    retval = str_and_flu_table[retval][diamonds];
+    retval = str_and_flu_table[retval][hearts  ];
 
-        if (fsm_hand > retval)
-	    retval = fsm_hand;
+    fsm_hand = rank_fsm_table[high_hand][n & FSM_MASK];
+    n >>= FSM_SHIFT;
 
-	fsm_hand = rank_fsm_table[high_hand][n & FSM_MASK];
-	n >>= FSM_SHIFT;
+    fsm_hand = rank_fsm_table[fsm_hand ][n & FSM_MASK];
+    n >>= FSM_SHIFT;
 
-	fsm_hand = rank_fsm_table[fsm_hand ][n & FSM_MASK];
-	n >>= FSM_SHIFT;
+    fsm_hand = rank_fsm_table[fsm_hand ][n & FSM_MASK];
+    n >>= FSM_SHIFT;
 
-	fsm_hand = rank_fsm_table[fsm_hand ][n & FSM_MASK];
-	n >>= FSM_SHIFT;
+    fsm_hand = rank_fsm_table[fsm_hand ][n];
 
-	fsm_hand = rank_fsm_table[fsm_hand ][n];
-
-	if (fsm_hand > retval)
-	    retval = fsm_hand;
-    }
+    if (fsm_hand > retval)
+        retval = fsm_hand;
 
     return retval;
 }
 
-#endif	/* !defined(__FISH_EVAL__) */
+#endif        /* !defined(__FISH_EVAL__) */
