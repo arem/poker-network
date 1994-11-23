fe2f1970e6a4f326b13c91bd6f1133c06dc99b1a
ctm <ctm@c4f34931-41c7-4605-b615-9bb9f479c57e>
1994-11-23 00:54:20 +0000

    cleaned up enum values.
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@69 c4f34931-41c7-4605-b615-9bb9f479c57e

--- ctm-eval/lib/makeswitch.h
+++ ctm-eval/lib/makeswitch.h
@@ -3,13 +3,18 @@
 
 typedef enum
 {
-  MAY_HAVE_FOUR_OF_A_KIND  = 0x1,
-  MAY_HAVE_FLUSH           = 0x2,
-  MAY_HAVE_STRAIGHT        = 0x4,
-  MAY_HAVE_FULL_HOUSE      = 0x8,
-  MAY_HAVE_THREE_OF_A_KIND = 0x10,
-  MAY_HAVE_TWO_PAIR        = 0x20,
-  MAY_HAVE_PAIR            = 0x40
+  CARDS_DEALT = 7
+} cards_dealt_t;
+
+typedef enum
+{
+  MAY_HAVE_FOUR_OF_A_KIND  = (1 << 0),
+  MAY_HAVE_FLUSH           = (1 << 1),
+  MUST_HAVE_STRAIGHT       = (1 << 2),
+  MAY_HAVE_FULL_HOUSE      = (1 << 3),
+  MAY_HAVE_THREE_OF_A_KIND = (1 << 4),
+  MUST_HAVE_AT_LEAST_PAIR  = (1 << 5),
+  MUST_HAVE_PAIR           = (1 << 6),
 } may_have_t;
 
 #define MAY_HAVE_COMBINATIONS 0x80
