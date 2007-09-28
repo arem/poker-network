968a4eacbfec69dd47e62430548c74b402da7e0a
tcort <tcort@c4f34931-41c7-4605-b615-9bb9f479c57e>
2007-09-28 01:12:10 +0000

    Add -v,--version and -h,--help command line options.
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@3405 c4f34931-41c7-4605-b615-9bb9f479c57e

--- tinypoker/bagelbot/main.c
+++ tinypoker/bagelbot/main.c
@@ -17,8 +17,11 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
+#include <getopt.h>
+
 #include "conf.h"
 #include "play.h"
+#include "main.h"
 #include "net.h"
 #include "byte.h"
 
@@ -45,8 +48,84 @@ int authenticate() {
                 return 0;
 }
 
-int main() {
-        /* Set the default config file path */
+
+/**
+ * Displays some usage information, command line parameters and whatnot.
+ * @param program the name of the program.
+ */
+void display_help(char *program) {
+	printf("Usage: %s [options]\n", program);
+	printf("Options:\n");
+	printf("    -h --help        Show this help message\n");
+	printf("    -v --version     Show version information\n");
+}
+
+/**
+ * Displays some version and copyright information upon request (-v or --version).
+ */
+void display_version() {
+	printf("%s v%1.1f\n", PROGRAM, VERSION);
+	printf("Copyright (C) 2005, 2007 Thomas Cort\n");
+	printf("This is free software; see the source for copying conditions.  There is NO\n");
+	printf("warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
+}
+
+
+/**
+ * A command line parser using getopts.
+ * @param argc The number of command line arguments coming in argv.
+ * @param argv The command line arguments.
+ * @return Returns 0 on success and non-zero when we want the program to terminate.
+ */
+int parse_args(int argc, char **argv) {
+	int option_index = 0, done = 0, c;
+
+	static struct option long_options[] = {
+		{      "help",       no_argument, 0, 'h'},
+		{   "version",       no_argument, 0, 'v'},
+		{0, 0, 0, 0}
+	};
+
+	while (!done) {
+		c = getopt_long(argc, argv, "hv", long_options, &option_index);
+		if (c < 0) {
+			break;
+		}
+
+		switch (c) {
+			case 'h':
+				display_help(argv[0]);
+				done = 1;
+				break;
+			case 'v':
+				display_version();
+				done = 1;
+				break;
+			default:
+				display_help(argv[0]);
+				done = 1;
+				break;
+		}
+	}
+
+	return done;
+}
+
+/**
+ * A command line poker bot.
+ * @param argc The number of command line arguments coming in argv.
+ * @param argv The command line arguments.
+ * @return Returns 0 on success and non-zero when we want the program to terminate.
+ */
+int main(int argc, char **argv) {
+	int rc;
+
+	if (argc < 1 || !argv || !argv[0]) {
+		printf("(%u:%s) Cannot determine program name from argv[0]\n", __FILE__, __LINE__);
+		return 1;
+	}
+
+	/* Set the default config file path */
 	configfile = (char*) malloc((strlen("/etc/bagelbot.conf") + 2) * sizeof(char));
 	if (!configfile) {
 		printf("malloc() failed!\n");
@@ -56,6 +135,11 @@ int main() {
 	snprintf(configfile,strlen("/etc/bagelbot.conf") + 1,"/etc/bagelbot.conf");
 
 	configure();
+
+	rc = parse_args(argc, argv);
+	if (rc) {
+		return rc;
+	}
 
 	if (!host || !pass || !port || !user) {
 		printf("Could not determine one or more configuration setting.\n");
