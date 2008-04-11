48d418e409660aec9dc838c1e385a4f0664e7084
tcort <tcort@c4f34931-41c7-4605-b615-9bb9f479c57e>
2008-04-11 00:55:49 +0000

    Add a SIGSEGV signal handler that logs a message asking the user to report a bug. Some useful debugging info is also logged along with a full stack trace (including function names and source location). The stack trace doesn't work so well when the segmentation fault occurs within a thread -- I may take it out at some point but it took so much work to code it, I can't justify throwing it out just yet.
    git-svn-id: http://svn.gna.org/svn/pokersource/trunk@3845 c4f34931-41c7-4605-b615-9bb9f479c57e

--- tinypoker/tinypokerd/src/signal.c
+++ tinypoker/tinypokerd/src/signal.c
@@ -20,8 +20,26 @@
 #include <libdaemon/dlog.h>
 #include <signal.h>
 #include <tinypoker.h>
+#include <stdio.h>
+#include <sys/utsname.h>
+#include <stdlib.h>
+#include <execinfo.h>
+#include <bfd.h>
+
+#ifndef __USE_GNU
+#define __USE_GNU
+#endif
+
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
+#include <dlfcn.h>
+
+#define MAX_STACK_FRAMES (128)
 
 #include "signal.h"
+#include "tinypokerd.h"
 
 /**
  * Global variable used to determine if the program should halt. Will it ever halt? Only Alan Turing knows.
@@ -69,6 +87,123 @@ void handle_sigint(int sig)
 }
 
 /**
+ * Signal handler for SIGSEGV
+ * @param sig signal to handle.
+ */
+void handle_sigsegv(int sig)
+{
+	Dl_info info;
+	struct utsname sys_uname;
+	void *frames[MAX_STACK_FRAMES];
+	asymbol **symbol_table;
+	char **bt_symbols;
+	size_t nbt_symbols, i;
+	int rc;
+	int symbol_table_size;
+	int number_of_symbols, j, found;
+
+	if (sig != SIGSEGV) {
+		return;
+	}
+
+	daemon_log(LOG_ERR, "=-=-=-=-=-=-=-=-=-=-=-=-=-= Segmentation fault =-=-=-=-=-=-=-=-=-=-=-=-=-=");
+	daemon_log(LOG_ERR, " ");
+	daemon_log(LOG_ERR, "A fatal error occured causing this application to crash. Please report the");
+	daemon_log(LOG_ERR, "entire error (everything between the 'Segmentation fault' lines) to the");
+	daemon_log(LOG_ERR, "following developer: Tom Cort <tom@tomcort.com>");
+	daemon_log(LOG_ERR, " ");
+
+	nbt_symbols = backtrace(frames, MAX_STACK_FRAMES);
+	bt_symbols = backtrace_symbols(frames, nbt_symbols);
+
+	for (i = 0; i < nbt_symbols; i++) {
+		rc = dladdr(frames[i], &info);
+		if (rc != 0 && info.dli_fname != NULL && info.dli_fname[0] != '\0') {
+			bfd *fd;
+
+			fd = bfd_openr(info.dli_fname, NULL);
+			if (fd == NULL) {
+				continue;
+			}
+
+			rc = bfd_check_format_matches(fd, bfd_object, NULL);
+			if (rc == 0) {
+				bfd_close(fd);
+				continue;
+			}
+
+			if (!(fd->flags & HAS_SYMS)) {
+				bfd_close(fd);
+				continue;
+			}
+
+			symbol_table_size = bfd_get_symtab_upper_bound(fd);
+			if (symbol_table_size <= 0) {
+				bfd_close(fd);
+				continue;
+			}
+
+			symbol_table = (asymbol **) malloc(symbol_table_size);
+			if (symbol_table == NULL) {
+				bfd_close(fd);
+				continue;
+			}
+
+			number_of_symbols = bfd_canonicalize_symtab(fd, symbol_table);
+			if (number_of_symbols <= 0) {
+				free(symbol_table);
+				symbol_table = NULL;
+				bfd_close(fd);
+				continue;
+			}
+
+			found = 0;
+			for (j = 0; j < number_of_symbols; j++) {
+				const char *file;
+				const char *func;
+				unsigned int lineno;
+
+				bfd_size_type sec_size;
+				sec_size = bfd_section_size(fd, symbol_table[j]->section);
+
+				if ((int) (frames[i]) < (int) (symbol_table[j]->section->vma)) {
+					continue;
+				}
+
+				if ((int) (frames[i]) >= (int) (symbol_table[j]->section->vma + sec_size)) {
+					continue;
+				}
+
+				bfd_find_nearest_line(fd, symbol_table[j]->section, symbol_table, (int) ((char *) frames[i] - symbol_table[j]->section->vma), &file, &func, &lineno);
+				if (file) {
+					daemon_log(LOG_ERR, "%16s() (%s:%d) [%p] <%s>", func, file, lineno, frames[i], info.dli_fname);
+				} else {
+					daemon_log(LOG_ERR, "%16s() [%p] <%s>", func, frames[i], info.dli_fname);
+				}
+				found = 1;
+				break;
+			}
+
+			bfd_close(fd);
+		}
+	}
+
+	free(bt_symbols);
+
+	daemon_log(LOG_ERR, " ");
+	daemon_log(LOG_ERR, "%s version %s was compiled at %s on %s", TINYPOKERD_NAME, TINYPOKERD_VERSION, __TIME__, __DATE__);
+
+	rc = uname(&sys_uname);
+	if (rc != -1) {
+		daemon_log(LOG_ERR, "%s version %s %s %s", sys_uname.sysname, sys_uname.version, sys_uname.release, sys_uname.machine);
+	}
+
+	daemon_log(LOG_ERR, " ");
+	daemon_log(LOG_ERR, "=-=-=-=-=-=-=-=-=-=-=-=-=-= Segmentation fault =-=-=-=-=-=-=-=-=-=-=-=-=-=");
+	exit(1);
+}
+
+/**
  *  installs signal handlers (mostly SIG_IGN)
  */
 void install_signal_handlers(void)
@@ -83,7 +218,7 @@ void install_signal_handlers(void)
 		 * (SIGUSR2)
 		 */
 
-		if (i != SIGCHLD && i != SIGQUIT && i != SIGINT && i != SIGKILL && i != SIGUSR2) {
+		if (i != SIGCHLD && i != SIGQUIT && i != SIGINT && i != SIGKILL && i != SIGUSR2 && i != SIGSEGV) {
 			signal(i, SIG_IGN);
 		}
 	}
@@ -91,4 +226,5 @@ void install_signal_handlers(void)
 	signal(SIGKILL, handle_sigkill);
 	signal(SIGQUIT, handle_sigquit);
 	signal(SIGINT, handle_sigint);
+	signal(SIGSEGV, handle_sigsegv);
 }
